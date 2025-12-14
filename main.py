4081ea35c8876f6fcd80c5b6b5c8eda9c8e8859f
Ahmet Ruçhan Avcı <aruchanavci01@gmail.com>
2025-12-15 01:50:55 +0300

    update 14.12.2025 Ver-1.0.6

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from langchain_core.messages import HumanMessage, SystemMessage
 
 load_dotenv() # Load environment variables from .env file
 
-#model_openai = ChatOpenAI(model="gpt-4o", temperature=0.1) # Initialize the ChatOpenAI model)
-model_gemini = ChatGoogleGenerativeAI(model='gemini-1.5-pro', temperature=0.1)
+model_openai = ChatOpenAI(model="gpt-4o", temperature=0.1) # Initialize the ChatOpenAI model)
+model_gemini = ChatGoogleGenerativeAI(model='gemini-2.5-flash', temperature=0.1)
 
 messages = [
     SystemMessage(content="You are a helpful assistant that translates English to Turkish."),
@@ -15,7 +15,11 @@ messages = [
 
 
 if __name__ == "__main__":
-    #response_openai = model_openai.invoke(messages) # Get the model's response to the messages
+    response_openai = model_openai.invoke(messages) # Get the model's response to the messages
     response_gemini = model_gemini.invoke(messages)
-    #print(response_openai)
+
+    print("OpenAI Response:")
+    print(response_openai)
+    print("\n*\n" * 50)
+    print("Gemini Response:")
     print(response_gemini)
