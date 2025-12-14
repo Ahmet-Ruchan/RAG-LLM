d6e562c8eae36471539757a3f2192894dba8fe7b
Ahmet Ruçhan Avcı <aruchanavci01@gmail.com>
2025-12-15 01:27:49 +0300

    update 14.12.2025 Ver-1.0.5

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage, SystemMessage
 
 load_dotenv() # Load environment variables from .env file
+
+#model_openai = ChatOpenAI(model="gpt-4o", temperature=0.1) # Initialize the ChatOpenAI model)
+model_gemini = ChatGoogleGenerativeAI(model='gemini-1.5-pro', temperature=0.1)
+
+messages = [
+    SystemMessage(content="You are a helpful assistant that translates English to Turkish."),
+    HumanMessage(content="Hello, how are you?")
+]
+
+
+if __name__ == "__main__":
+    #response_openai = model_openai.invoke(messages) # Get the model's response to the messages
+    response_gemini = model_gemini.invoke(messages)
+    #print(response_openai)
+    print(response_gemini)
